9b381274ae8019c6071a1c67b695811c66839a49
bql20000 <bql20000@gmail.com>
2021-03-17 22:32:02 +0700

    Using functions returning widgets to create dynamiz size widget list

--- lib/main.dart
+++ lib/main.dart
@@ -23,24 +23,28 @@ class _QuoteListState extends State<QuoteList> {
   Widget quoteTemplate(quote) {
     return Card(
       margin: EdgeInsets.fromLTRB(16, 16, 0, 0),
-      child: Column(
-        children: [
-          Text(
-            quote.text,
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.grey[600],
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[600],
+              )
+            ),
+            SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[600],
+              )
             )
-          ),
-          SizedBox(height: 6),
-          Text(
-            quote.author,
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.grey[600],
-            )
-          )
-        ],
+          ],
+        ),
       )
     );
   }
@@ -55,7 +59,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
