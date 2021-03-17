011c05837eda9c78ba58f26400983eac1ca1b134
bql20000 <bql20000@gmail.com>
2021-03-17 13:06:35 +0700

    Create a simple profile ID card

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,94 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  home: LongCard(),
 ));
 
-class Home extends StatelessWidget {
+class LongCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.orange[100],
       appBar: AppBar(
-        title: Text('Hi'),
+        title: Text('Long ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.orange,
-      ),
-      // body: Container(
-      //   padding: EdgeInsets.fromLTRB(10, 20, 30, 40),
-      //   margin: EdgeInsets.all(30),
-      //   color: Colors.grey[400],
-      //   child: Text('Hello')
-      // ),
-      body: Row(
-        children: [
-          Expanded (
-              flex: 3,
-              child: Image.asset('assets/image0.jpeg'),
-          ),
-          Expanded (
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.cyan,
-              child: Text('1'),
-            ),
-          ),
-          Expanded (
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.pink,
-              child: Text('2'),
-            ),
-          ),
-          Expanded (
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.amber,
-              child: Text('3'),
-            ),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child:  Text('click'),
         backgroundColor: Colors.orangeAccent,
+        elevation: 0,
       ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/image0.jpeg'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.lightBlue
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.blue,
+                letterSpacing: 2
+              )
+            ),
+            SizedBox(height: 10),
+            Text(
+                'Long',
+                style: TextStyle(
+                    color: Colors.redAccent,
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+            SizedBox(height: 30),
+            Text(
+                'SCHOOL',
+                style: TextStyle(
+                    color: Colors.blue,
+                    letterSpacing: 2
+                )
+            ),
+            SizedBox(height: 10),
+            Text(
+                'UET',
+                style: TextStyle(
+                    color: Colors.redAccent,
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.blueAccent,
+                ),
+                SizedBox(width: 5),
+                Text(
+                  'bql20000@gmail.com',
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontSize: 18,
+                    letterSpacing: 1
+                  )
+                )
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
+
 
 
