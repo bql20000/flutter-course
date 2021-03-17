a9c437e65d65737538bb532f513f43405cdbe31e
bql20000 <bql20000@gmail.com>
2021-03-17 08:37:02 +0700

    Scaffold, AppBar, Colors, Fonts, Stateless Widget & Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp();
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My girl friend'),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+        child: Text(
+            'Yo Yo Yo Yo Long',
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.green,
+                fontFamily: 'IndieFlower'
+            )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child:  Text('click'),
+        backgroundColor: Colors.orangeAccent,
+      ),
+    );
+  }
+}
 
 
